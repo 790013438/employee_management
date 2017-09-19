baa936a55ee28a5ac2cb227c81bf3f0018374d3c
790013438 <790013438@qq.com>
2017-09-19 17:48:26 +0800

    dept

---
+++ src/main/java/snippets/jee/dao/DeptDAO.java
@@ -0,0 +1,6 @@
+package snippets.jee.dao;
+
+public interface DeptDAO {
+
+    public int countEmpByNo (Integer no);
+}
---
+++ src/main/java/snippets/jee/dao/impl/DeptDAOImpl.java
@@ -0,0 +1,12 @@
+package snippets.jee.dao.impl;
+
+import snippets.jee.dao.DeptDAO;
+
+public class DeptDAOImpl implements DeptDAO {
+
+    @Override
+    public int countEmpByNo(Integer no) {
+        return 0;
+    }
+
+}
