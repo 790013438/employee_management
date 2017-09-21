a388713b346b01a104b85beea66a1acde7fd6c58
790013438 <790013438@qq.com>
2017-09-21 10:07:42 +0800

    使用工具类，简化user material interview aim

--- src/main/java/snippets/jee/dao/impl/UserDAOImpl.java
+++ src/main/java/snippets/jee/dao/impl/UserDAOImpl.java
@@ -1,13 +1,12 @@
 package snippets.jee.dao.impl;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import snippets.jee.dao.UserDAO;
 import snippets.jee.dto.UserDTO;
+import snippets.jee.util.DBResourceManager;
 
 /**
  * material interview fulfill
@@ -16,50 +15,39 @@ import snippets.jee.dto.UserDTO;
  */
 public class UserDAOImpl implements UserDAO {
 
-    static {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
+    private static final String SELECT_USER_SQL = "select password, email from tb_user where username=?";
+    private static final String INSERT_USER_SQL = "insert into tb_user values (?,?,?)";
 
     @Override
     public UserDTO getUser(String name) {
-        UserDTO user = null;
-        try (Connection connection = DriverManager.getConnection(
-                    "jdbc:mysql:///hrs", "user1", "37934bit")) {
-            PreparedStatement stmt = connection.prepareStatement(
-                    "select password, email from tb_user where username=?");
-            stmt.setString(1, name);
-            ResultSet rs = stmt.executeQuery();
+        Connection connection = DBResourceManager.openConnection();
+        ResultSet rs = DBResourceManager.executeQuery(connection, SELECT_USER_SQL, name);
+        try {
+            UserDTO user = null;
             if (rs.next()) {
                 user = new UserDTO();
                 user.setUsername(name);
                 user.setPassword(rs.getString("password"));
                 user.setEmail(rs.getString("email"));
             }
+            return user;
         } catch (SQLException e) {
             e.printStackTrace();
+            throw new RuntimeException("解析结果集时出现异常38:)", e);
+        } finally {
+            DBResourceManager.closeConnection(connection);
         }
-        return user;
-
     }
 
     @Override
     public boolean addUser(UserDTO userDTO) {
-        try (Connection connection = DriverManager.getConnection(
-                    "jdbc:mysql:///hrs", "root", "123456")) {
-            PreparedStatement stmt = connection.prepareStatement(
-                    "insert into tb_user values (?,?,?)");
-            stmt.setString(1, userDTO.getUsername());
-            stmt.setString(2, userDTO.getPassword());
-            stmt.setString(3, userDTO.getEmail());
-            return stmt.executeUpdate() == 1;
-        } catch (SQLException e) {
-            e.printStackTrace();
+        Connection connection = DBResourceManager.openConnection();
+        try {
+            return DBResourceManager.executeUpdate(connection, INSERT_USER_SQL, 
+                    userDTO.getUsername(), userDTO.getPassword(), userDTO.getEmail()) == 1;
+        } finally {
+            DBResourceManager.closeConnection(connection);
         }
-        return false;
     }
 
 }
