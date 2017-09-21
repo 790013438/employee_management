f2027027af86c3aa3aebf3d0ab9e267df9ee8153
790013438 <790013438@qq.com>
2017-09-21 16:44:00 +0800

    department dao

--- src/main/java/snippets/jee/dao/DeptDAO.java
+++ src/main/java/snippets/jee/dao/DeptDAO.java
@@ -1,6 +1,42 @@
 package snippets.jee.dao;
 
+import java.util.List;
+
+import snippets.jee.dto.Dept;
+
 public interface DeptDAO {
 
-    public int countEmpByNo (Integer no);
+    /**
+     * 保存部门
+     * @param dept 部门对象
+     * @return 保存成功返回true否则返回false
+     */
+    boolean save(Dept dept);
+
+    /**
+     * 删除部门
+     * @param no 部门编号
+     * @return 删除成功返回true否则返回false
+     */
+    boolean deleteByNo(Integer no);
+
+    /**
+     * 更新部门
+     * @param dept 部门对象
+     * @return 更新成功返回true否则返回false
+     */
+    boolean update(Dept dept);
+
+    /**
+     * 查询所有部门
+     * @return 保存部门对象的列表容器
+     */
+    List<Dept> findAll();
+
+    /**
+     * 查询部门人数
+     * @param no 部门编号
+     * @return 部门人数
+     */
+    int countEmpByNo(Integer no);
 }
--- src/main/java/snippets/jee/dao/impl/DeptDAOImpl.java
+++ src/main/java/snippets/jee/dao/impl/DeptDAOImpl.java
@@ -1,12 +1,89 @@
 package snippets.jee.dao.impl;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import snippets.jee.dao.DeptDAO;
+import snippets.jee.dto.Dept;
+import snippets.jee.util.DBResourceManager;
 
 public class DeptDAOImpl implements DeptDAO {
 
+    private static final String SELECT_ALL_DEPT_SQL = "select dno, dname, dloc from tb_dept";
+    private static final String INSERT_DEPT_SQL = "insert into tb_dept values (?,?,?)";
+    private static final String DELETE_DEPT_SQL = "delete from tb_dept where dno=?";
+    private static final String UPDATE_DEPT_SQL = "update tb_dept set dname=?, dloc=? where dno=?";
+    private static final String COUNT_EMP_SQL = "select count(eno) from tb_emp where dno=?";
+
     @Override
-    public int countEmpByNo(Integer no) {
-        return 0;
+    public boolean save(Dept dept) {
+        Connection connection = DBResourceManager.openConnection();
+        try {
+            return DBResourceManager.executeUpdate(connection, INSERT_DEPT_SQL, 
+                    dept.getNo(), dept.getName(), dept.getLocation()) == 1;
+        } finally {
+            DBResourceManager.closeConnection(connection);
+        }
     }
 
+    @Override
+    public boolean deleteByNo(Integer no) {
+        Connection connection = DBResourceManager.openConnection();
+        try {
+            return DBResourceManager.executeUpdate(connection, DELETE_DEPT_SQL, no) == 1;
+        } finally {
+            DBResourceManager.closeConnection(connection);
+        }
+    }
+
+    @Override
+    public boolean update(Dept dept) {
+        Connection connection = DBResourceManager.openConnection();
+        try {
+            return DBResourceManager.executeUpdate(connection, UPDATE_DEPT_SQL, 
+                    dept.getName(), dept.getLocation(), dept.getNo()) == 1;
+        } finally {
+            DBResourceManager.closeConnection(connection);
+        }
+    }
+
+    @Override
+    public List<Dept> findAll() {
+        List<Dept> deptList = new ArrayList<>();
+        Connection connection = DBResourceManager.openConnection();
+        ResultSet rs = DBResourceManager.executeQuery(connection, SELECT_ALL_DEPT_SQL);
+        try {
+            while (rs.next()) {
+                Dept dept = new Dept();
+                dept.setNo(rs.getInt("dno"));
+                dept.setName(rs.getString("dname"));
+                dept.setLocation(rs.getString("dloc"));
+                deptList.add(dept);
+            }
+            return deptList.size() > 0 ? deptList : Collections.emptyList();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("70:)", e);
+        } finally {
+            DBResourceManager.closeConnection(connection);
+        }
+    }
+
+    @Override
+    public int countEmpByNo(Integer no) {
+        Connection connection = DBResourceManager.openConnection();
+        ResultSet rs = DBResourceManager.executeQuery(connection, COUNT_EMP_SQL, no);
+        try {
+            return rs.next() ? rs.getInt(1) : 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("84:)", e);
+        } finally {
+            DBResourceManager.closeConnection(connection);
+        }
+    }
 }
