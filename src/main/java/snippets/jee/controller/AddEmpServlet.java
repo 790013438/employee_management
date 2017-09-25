8f3d8d4f2be9001dd67cb54658b0ad1b467761ca
790013438 <790013438@qq.com>
2017-09-25 14:55:39 +0800

    图片压缩

--- src/main/java/snippets/jee/controller/AddEmpServlet.java
+++ src/main/java/snippets/jee/controller/AddEmpServlet.java
@@ -1,9 +1,11 @@
 package snippets.jee.controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Date;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,22 +15,25 @@ import snippets.jee.dto.Dept;
 import snippets.jee.dto.Emp;
 import snippets.jee.util.CommonUtil;
 
-@WebServlet("/addEmp")
+@WebServlet(value = "/addEmp", loadOnStartup = 1)
+@MultipartConfig(maxFileSize = 10 * 1024 * 1024)
 public class AddEmpServlet extends BaseServlet {
+    private static final int DEFAULT_IMAGE_WIDTH = 200;
+    private static final int DEFAULT_IMAGE_HEIGHT = 150;
 
     private static final long serialVersionUID = 1L;
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        int no = Integer.parseInt(req.getParameter("no"));
-        String name = req.getParameter("name");
-        boolean sex = req.getParameter("sex").equals("1");
-        String job = req.getParameter("job");
-        double salary = Double.parseDouble(req.getParameter("salary"));
-        String hireDateStr = req.getParameter("hiredate");
+    protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse resp) throws ServletException, IOException {
+        int no = Integer.parseInt(httpServletRequest.getParameter("no"));
+        String name = httpServletRequest.getParameter("name");
+        boolean sex = httpServletRequest.getParameter("sex").equals("1");
+        String job = httpServletRequest.getParameter("job");
+        double salary = Double.parseDouble(httpServletRequest.getParameter("salary"));
+        String hireDateStr = httpServletRequest.getParameter("hiredate");
         System.out.println(hireDateStr);
         Date hireDate = CommonUtil.stringToDate("yyyy-MM-dd", hireDateStr);
-        String tel = req.getParameter("tel");
+        String tel = httpServletRequest.getParameter("tel");
         Emp emp = new Emp();
         emp.setNo(no);
         emp.setName(name);
@@ -40,24 +45,23 @@ public class AddEmpServlet extends BaseServlet {
         emp.setTel(tel);
         Emp mgr = new Emp();
         emp.setMgr(mgr);
-        Part part = req.getPart("photo");
+        Part part = httpServletRequest.getPart("photo");
         if (part.getSize() > 0) {
-            String originalName = part.getSubmittedFileName();
-            String newFilename = CommonUtil.getUniqueFilename() +
-                CommonUtil.getFilenameSuffix(originalName);
-            String path = req.getServletContext().getRealPath("/images");
-            part.write(path + "/" + newFilename);
+            String newFilename = CommonUtil.getUniqueFilename() + "png";
+            String path = httpServletRequest.getServletContext().getRealPath("/images");
+            File file = new File(path + "/" + newFilename);
+            CommonUtil.compressImage(part.getInputStream(), file, DEFAULT_IMAGE_WIDTH, DEFAULT_IMAGE_HEIGHT);
             emp.setPhoto(newFilename);
         }
-        int deptNo = Integer.parseInt(req.getParameter("dno"));
+        int deptNo = Integer.parseInt(httpServletRequest.getParameter("dno"));
         Dept dept = new Dept();
         dept.setNo(deptNo);
         emp.setDept(dept);
         if (getEmpService().addNewEmp(emp)) {
             resp.sendRedirect("emp?no=" + deptNo);
         } else {
-            req.setAttribute("hint", "添加失败!!!");
-            req.getRequestDispatcher("add_emp.jsp").forward(req, resp);
+            httpServletRequest.setAttribute("hint", "添加失败!!!");
+            httpServletRequest.getRequestDispatcher("add_emp.jsp").forward(httpServletRequest, resp);
         }
     }
 
--- src/main/webapp/add_emp.jsp
+++ src/main/webapp/add_emp.jsp
@@ -1,9 +1,18 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>Insert title here</title>
+        <style>
+            #photoPreview {
+                border: 1px dashed #ccc;
+                display: block;
+                width: 150px;
+                height: 150px;
+            }
+        </style>
     </head>
     <body>
         <h1>新增员工</h1>
@@ -22,12 +31,38 @@
             <input type="number" name="salary" required placeholder="请输入员工月薪">
             <br><br>
             <input type="date" name="hiredate">
-            <input type="file" name="photo">
+            <br><br>
+            <input id="photo" type="file" name="photo">
+            <img id="photoPreview" src="" alt="">
             <br><br>
             <input type="tel" name="tel" placeholder="请输入员工电话">
+            <br><br>
             <input type="hidden" name="dno" value="${param.dno}">
             <input type="submit" value="确定">
         </form>
         <a href="dept">返回部门列表</a>
+        <script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
+        <script>
+            $(function() {
+                $('#photo').on('change', function() {
+                    if (window.FileReader) {
+                        var file = this.files[0];
+                        if (/image\/\w+/.test(file.type)) {
+                            var fr = new FileReader();
+                            fr.onloadend = function(evt) { 
+                                $('#photoPreview').attr('src', evt.target.result);
+                            };
+                            fr.readAsDataURL(file);
+                        } else {
+                            $(this).val('');
+                            $('#photoPreview').attr('src', '');
+                            alert("请选择图片文件");
+                        }
+                    } else {
+                        alert("浏览器不支持文件预览!");
+                    }
+                });
+            });
+        </script>
     </body>
 </html>
