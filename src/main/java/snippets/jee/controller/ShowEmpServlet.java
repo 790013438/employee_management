5e961c1250cabebae7eaeebedc9ef5088e72a311
790013438 <790013438@qq.com>
2017-09-26 17:08:32 +0800

    奇怪的操作

--- src/main/java/snippets/jee/controller/ShowEmpServlet.java
+++ src/main/java/snippets/jee/controller/ShowEmpServlet.java
@@ -1,6 +1,7 @@
 package snippets.jee.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,15 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import snippets.jee.dto.Dept;
-import snippets.jee.dto.EmpDTO;
-import snippets.jee.util.PageBean;
 
 @WebServlet("/emp")
 public class ShowEmpServlet extends BaseServlet {
 
     private static final long serialVersionUID = 1L;
     private static final int DEFAULT_PAGE = 1;
-    private static final int DEFAULT_SIZE = 5;
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -37,12 +35,15 @@ public class ShowEmpServlet extends BaseServlet {
                 } catch (NumberFormatException e) {
                 }
             }
-            int size = DEFAULT_SIZE;
-            PageBean<EmpDTO> pageBean = getEmpService().listAllEmpsByDeptNo(dept.getNo(), page, size);
-            req.setAttribute("empList", pageBean.getDataModel());
-            req.setAttribute("totalPage", pageBean.getTotalPage());
-            req.setAttribute("currentPage", pageBean.getCurrentPage());
-            req.getRequestDispatcher("emp.jsp").forward(req, resp);
+            // 1. 将对象转换成JSON格式的字符串返回给浏览器
+            // { 'empList': [{'no': 1122, 'name': '骆昊',  ...}, {}, ...], 'totalPage': 4, 'currentPage': 2, 'pageSize': 5 }
+            // Gson / Fastjson
+            String jsonStr = "";
+            // 2. 把JSON字符串输出到浏览器通过MIME类型告诉浏览器这里是JSON格式
+            resp.setContentType("application/json;charset=utf-8");
+            PrintWriter printWriter = resp.getWriter();
+            printWriter.write(jsonStr);
+            printWriter.close();
         }
     }
 }
