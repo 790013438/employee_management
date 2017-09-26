d219cc3b3897d95847606d098b309d730e34ecc9
790013438 <790013438@qq.com>
2017-09-26 20:20:24 +0800

    employee显示 发json

--- src/main/java/snippets/jee/controller/ShowEmpServlet.java
+++ src/main/java/snippets/jee/controller/ShowEmpServlet.java
@@ -8,13 +8,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.alibaba.fastjson.JSON;
+
 import snippets.jee.dto.Dept;
+import snippets.jee.dto.EmpDTO;
+import snippets.jee.util.PageBean;
 
 @WebServlet("/emp")
 public class ShowEmpServlet extends BaseServlet {
 
     private static final long serialVersionUID = 1L;
     private static final int DEFAULT_PAGE = 1;
+    private static final int DEFAULT_SIZE = 5;
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -29,7 +34,10 @@ public class ShowEmpServlet extends BaseServlet {
                 } catch (NumberFormatException e) {
                 }
             }
-            String jsonStr = "JSON.toJSONString(pageBean)";
+            int size = DEFAULT_SIZE;
+            PageBean<EmpDTO> pageBean = getEmpService().listAllEmpsByDeptNo(dept.getNo(), page, size);
+            // 1. 将对象转换成JSON格式的字符串返回给浏览器
+            String jsonStr = JSON.toJSONString(pageBean);
             // 2. 把JSON字符串输出到浏览器通过MIME类型告诉浏览器这里是JSON格式
             resp.setContentType("application/json;charset=utf-8");
             PrintWriter printWriter = resp.getWriter();
