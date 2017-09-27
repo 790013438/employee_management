9d11257639ae6a9f360ff1fc2363d4c143bd7887
790013438 <790013438@qq.com>
2017-09-27 17:28:53 +0800

    修改

--- src/main/java/snippets/jee/controller/DelEmpServlet.java
+++ src/main/java/snippets/jee/controller/DelEmpServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import snippets.jee.dto.Dept;
+
 @WebServlet(value="/delEmp")
 public class DelEmpServlet extends BaseServlet {
 
@@ -15,13 +17,13 @@ public class DelEmpServlet extends BaseServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String noString = req.getParameter("no");
+        Dept deptDTO = (Dept) req.getSession().getAttribute("dept");
         if (noString != null) {
             int no = Integer.parseInt(noString);
-            if (getDeptService().removeDeptByNo(no)) {
-                resp.sendRedirect("emp");
-            } else {
+            if (!getEmpService().removeEmpByNo(no)) {
                 req.setAttribute("hint", "删除员工失败");
             }
+            resp.sendRedirect("toEmp?no=" + deptDTO.getId());
         }
     }
 
