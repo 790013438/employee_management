f2d718fbfd0f8f623c0710c9bdd4a13e559885bb
790013438 <790013438@qq.com>
2017-09-27 15:22:27 +0800

    webservlet少了斜杠

--- src/main/java/snippets/jee/controller/DelEmpServlet.java
+++ src/main/java/snippets/jee/controller/DelEmpServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(value="delEmp")
+@WebServlet(value="/delEmp")
 public class DelEmpServlet extends BaseServlet {
 
     private static final long serialVersionUID = 1L;
